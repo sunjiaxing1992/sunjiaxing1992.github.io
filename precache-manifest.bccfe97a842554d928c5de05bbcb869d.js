8227b15be0999fea54f0ffe67244d0f709429294
sunjiaxing1992 <sunjiaxing1992@gmail.com>
2018-10-30 14:52:20 +0800

    try

---
+++ precache-manifest.bccfe97a842554d928c5de05bbcb869d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8edf4cc7f9a896da9ab3",
+    "url": "/static/js/main.8edf4cc7.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "8edf4cc7f9a896da9ab3",
+    "url": "/static/css/main.e999fca3.chunk.css"
+  },
+  {
+    "revision": "40a1b0d9a9c684bdfeb4cf4e9792fc2a",
+    "url": "/index.html"
+  }
+];
